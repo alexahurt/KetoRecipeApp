f3169713aeffba4ea4800a4cb04e94335caf5504
Alexa Hurt <alexacorinnehurt@gmail.com>
2021-11-24 19:48:10 -0500

    layout update

--- KetoRecipeApp.WebMVC/Controllers/FavoriteRecipesController.cs
+++ KetoRecipeApp.WebMVC/Controllers/FavoriteRecipesController.cs
@@ -30,71 +30,22 @@ namespace KetoRecipeApp.WebMVC.Controllers
             return View(model);
         }
 
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(FavoriteRecipesCreate model)
         {
-            if (ModelState.IsValid) return View(model);
-
-            var service = CreateFavoriteRecipesService();
-
-            if (service.CreateFavoriteRecipes(model))  //this method does exist in favoriterecipesservice
+            if (ModelState.IsValid)
             {
-                TempData["SaveResult"] = "Your recipe was added as a favorite.";
-                return RedirectToAction("Index");
-            };
-
-            ModelState.AddModelError("", "Recipe could not be created.");
-
-            return View(model);
-
-        }
-
-        public ActionResult Details(int id)
-        {
-            var svc = CreateFavoriteRecipesService();
-            var model = svc.GetFavoriteRecipesById(id);  //this method does exist in favorite recipes service page
-
-            return View(model);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            var service = CreateFavoriteRecipesService();
-            var detail = service.GetFavoriteRecipesById(id);  //this method does exist
-            var model =
-                new FavoriteRecipes
-                {
-                    Id = detail.FavoriteRecipesId,
-                    RecipeTitle = detail.RecipeTitle,
-                    Source = detail.Source,
-                    UserId = detail.UserId
-                };
-
-            return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, FavoriteRecipesEdit model)
-        {
-            if (!ModelState.IsValid) return View(model);
-
-            if (model.FavoriteRecipesId != id)
-            {
-                ModelState.AddModelError("", "Id Mismatch");
-                return View(model);
-            }
-            var service = CreateFavoriteRecipesService();
-
-            if (service.UpdateFavoriteRecipes(model))
-            {
-                TempData["SaveResult"] = "Your Recipe was updated.";
+                var service = CreateFavoriteRecipesService();
+                if (service.CreateFavoriteRecipes(model))
+                    TempData["SaveResult"] = "This recipe was added as a favorite.";
                 return RedirectToAction("Index");
             }
 
-            ModelState.AddModelError("", "Your recipe could not be updated.");
+            ModelState.AddModelError("", "Recipe could not be added to your favorites.");
             return View(model);
+
         }
 
         public ActionResult Delete(int id)
@@ -115,6 +66,7 @@ namespace KetoRecipeApp.WebMVC.Controllers
             TempData["SaveResult"] = "Your recipe was deleted";
             return RedirectToAction("Index");
         }
+        
 
         private FavoriteRecipesService CreateFavoriteRecipesService()
         {
